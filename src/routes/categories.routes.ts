b883ac8cbda41798fe28bd3fc57c41294605df97
JeanAlves2021 <jean.pdolsky2@gmail.com>
2022-04-08 12:13:37 -0300

    first commit to the rentalx_app

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRepository = new CategoriesRepository();
+
+const categoriesRoutes = Router();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
